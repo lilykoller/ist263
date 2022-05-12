97538f570bbcf4ed411ce22edae616c2a85ae5a7
lilykoller <lmkoller@syr.edu>
2022-05-11 21:43:30 -0400

    hi

---
+++ project6/script.js
@@ -0,0 +1,16 @@
+window.addEventListener("load", function(){
+    this.setTimeout(
+        function open(event){
+            document.querySelector(".popup").style.
+            display = "block";
+        },
+        1000
+    )
+});
+
+
+document.querySelector("#close").addEventListener
+("click",function(){
+    document.querySelector(".popup").
+    style.display = "none";
+});
